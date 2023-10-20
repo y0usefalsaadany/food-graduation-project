f8718ce20c4af8214c2e5d2738e0bb11a8133db4
unknown <yousefalsaadany5@gmail.com>
2023-10-20 15:13:20 +0300

    add category select

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
     }
     public function show($id)
     {
-        $categories = Category::find($id)->with('products')->latest();
+        $categories = Category::find($id)->with('products');
         return response()->json([
             "data" => $categories
         ]);
